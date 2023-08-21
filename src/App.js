ac1d287ff67e0321487a59e3aad830b0564a9bfe
Jose Bravo <josebravowebdev@gmail.com>
2023-08-21 00:18:09 -0400

    Markdown Previewer

---
+++ src/App.js
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+import './App.scss';
+
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import remarkBreaks from 'remark-breaks';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
+function MarkdownPreviewer() {
+  const [markdown, setMarkdown] = useState(`
+  # This is a heading!
+  ## This is a sub-heading...
+  ### And here's some other cool stuff:
+
+  Heres some code, \`<div></div>\`, between 2 backticks.
+
+  \`\`\`
+  // this is multi-line code:
+
+  function anotherExample(firstLine, lastLine) {
+    if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+      return multiLineCode;
+    }
+  }
+  \`\`\`
+
+  You can also make text **bold**... whoa!
+  Or _italic_.
+  Or... wait for it... **_both!_**
+  And feel free to go crazy ~~crossing stuff out~~.
+
+  There's also [links](https://www.freecodecamp.org), and
+  > Block Quotes!
+
+  And if you want to get really crazy, even tables:
+
+  Wild Header | Crazy Header | Another Header?
+  ------------ | ------------- | -------------
+  Your content can | be here, and it | can be here....
+  And here. | Okay. | I think we get it.
+
+  - And of course there are lists.
+    - Some are bulleted.
+       - With different indentation levels.
+          - That look like this.
+
+
+  1. And there are numbered lists too.
+  1. Use just 1s if you want!
+  1. And last but not least, let's not forget embedded images:
+
+  ![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)`);
+
+  return (
+    <>
+      <div className="editor-container">
+        <textarea
+          id="editor"
+          value={markdown}
+          onChange={(e) => setMarkdown(e.target.value)}
+        />
+      </div>
+      <div className="preview-container">
+        <div id="preview">
+          <ReactMarkdown
+            children={markdown}
+            remarkPlugins={[remarkGfm, remarkBreaks]}
+            components={{
+              code({node, inline, className, children, ...props}) {
+                const match = /language-(\w+)/.exec(className || '');
+                return !inline && match ? (
+                  <SyntaxHighlighter
+                    children={String(children).replace(/\n$/, '')}
+                    style={dark}
+                    language={match[1]} 
+                    PreTag="div"  
+                    {...props}
+                  />
+                ) : (
+                  inline ? <code className={className} {...props}>{children}</code> : <>{children}</>
+                );
+              },
+            }}
+          />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default MarkdownPreviewer;
